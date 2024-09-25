1157a92ad9e021088021e84f00b7d675a820af3b
Pranjal-1307 <146635532+Pranjal-1307@users.noreply.github.com>
2024-09-25 15:42:15 +0530

    Update api_services.dart

--- lib/service/api_services.dart
+++ lib/service/api_services.dart
@@ -1,8 +1,10 @@
 import 'dart:convert';
 import 'dart:developer';
+import 'package:flutter/foundation.dart';
 import 'package:showbox/common/utils.dart';
+import 'package:showbox/models/search_model.dart';
 import 'package:showbox/models/tv_series_model.dart';
-import 'package:showbox/models/upcomingMovies.dart';
+import 'package:showbox/models/Upcoming_Movies.dart';
 import 'package:http/http.dart' as http;
 
 const baseUrl="https://api.themoviedb.org/3/";
@@ -48,6 +50,28 @@ class ApiServices {
       return TvsSeriesModel.fromJson(jsonDecode(response.body));
     }
     throw Exception("Now Playing Top Rated TV Series Failed To Load");
+  }
+
+  Future<SearchModel> getSearchMovie(String searchText) async {
+    endPoint = "search/movie?query=$searchText";
+    final url = "$baseUrl$endPoint";
+
+    if (kDebugMode) {
+      print("Search URL: $url");
+    }
+
+    final response = await http.get(
+      Uri.parse(url),
+      headers: {
+        'Authorization': "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1OWQ0NmE2NGM5OWQzNjQwYTgxYzdiNTBiOGZiOTNmMCIsIm5iZiI6MTcyNzEwMDIwMy41Mzg0OTUsInN1YiI6IjY2YjBhZWNhNzQ0M2YyNzk3OWJkZDZlZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.1lTrBapKQZ3egFJeybU_jWG_4P4xsq7YsRnIoLo9orY",
+      },
+    );
+
+    if (response.statusCode == 200) {
+      log("Success: Search Results Fetched");
+      return SearchModel.fromJson(jsonDecode(response.body));
+    }
+    throw Exception("Failed to load search results");
   }
 }
 
